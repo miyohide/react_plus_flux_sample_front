f86e0067212879c95a63703fe2f7371e2f284e3b
miyohide <miyohide@gmail.com>
2015-07-24 21:19:05 +0900

    add constants

---
+++ scripts/constants/SmallConstants.js
@@ -0,0 +1,31 @@
+var keyMirror = require("keymirror");
+
+var APIRoot = "http://localhost:3000"
+
+module.exports = {
+    APIEndpoints: {
+        LOGIN:        APIRoot + "/v1/login";
+        REGISTRATION: APIRoot + "/v1/users";
+        STORIES:      APIRoot + "/v1/stories";
+    },
+
+    PayloadSources: keyMirror({
+        SSERVER_ACTION: null,
+        VIEW_ACTION:    null
+    }),
+
+    ActionTypes: keyMirror({
+        LOGIN_REQUEST:  null,
+        LOGIN_RESPONSE: null,
+
+        REDIRECT: null,
+
+        LOAD_STORIES:          null,
+        RECEIVE_STORIES:       null,
+        LOAD_STORY:            null,
+        RECEIVE_STORY:         null,
+        CREATE_STORY:          null,
+        RECEIVE_CREATED_STORY: null
+    })
+};
+
