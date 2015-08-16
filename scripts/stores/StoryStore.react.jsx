a1688b0758cb5b95e2fa7fbff0c49b4db81fe601
miyohide <miyohide@gmail.com>
2015-08-16 15:13:59 +0900

    add addChangeListener function

--- scripts/stores/StoryStore.react.jsx
+++ scripts/stores/StoryStore.react.jsx
@@ -16,6 +16,10 @@ var StoryStore = assign({}, EventEmitter.prototype, {
         this.emit(CHANGE_EVENT);
     },
 
+    addChangeListener: function(callback) {
+        this.on(CHANGE_EVENT, callback);
+    },
+
     removeChangeListener: function(callback) {
         this.removeListener(CHANGE_EVENT, callback);
     },
